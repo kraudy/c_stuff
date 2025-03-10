6479fbca4bdd1ee7b8219688dfef2451dcd199c0
Roberto Torrez <robkraudy@gmail.com>
2025-03-10 13:05:52 -0600

    Simple matrix stuff
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/basic_stuff.c
+++ beejs_guide/basic_stuff.c
@@ -25,6 +25,8 @@ void pointers(int* i){
 void arrays(void){
   puts("\narrays");
   float f[4]; 
+  int a[5] = {1,2,3,4,5};
+  int b[100] = {0}; // This wil set all elements to zero
   f[0] = 3.1638; f[1] = 1.2345;
   f[2] = 1.6808; f[3] = 2.7898;
   for (int i=0; i<4; i++){
@@ -41,6 +43,22 @@ void size_stuff(void){
   printf("%zu\n", sizeof(x) / sizeof(int));
 }
 
+void matrix_stuff(void){
+  puts("\nmatrix_stuff");
+  int row, col;
+  int a[2][5] = {
+    {0, 1, 2, 3, 4},
+    {5, 6, 7, 8, 9}
+  };
+
+  for (row=0; row<2; row++){
+    for (col=0; col<5; col++){
+      printf("(%d, %d) = %d\n", row, col, a[row][col]);
+    }
+  }
+
+}
+
 
 int main (void){
   int i = 2;   
@@ -49,5 +67,6 @@ int main (void){
   pointers(&i);
   arrays();
   size_stuff();
+  matrix_stuff();
 
 }
