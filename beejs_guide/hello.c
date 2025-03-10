9d505e048d28b37afa68ebad12576bb7dde7cedb
Roberto Torrez <robkraudy@gmail.com>
2025-03-10 06:43:25 -0600

    Refacto stuff to functions
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/hello.c
+++ beejs_guide/hello.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int main (void){
-  bool x = true;
-  int i = 2; float f = 3.14;
+void pointers(int* i){
   int *p;
   char *s = "Hello, world !";
 
-  p = &i;
+  p = i;
 
-  printf("%s i  = %d and f = %f \n", s, i, f);
+  printf("%s i  = %d and f = %f \n", s, i);
   printf("i addres: %p \n", (void *) p);
   printf("%zu\n", sizeof p); //size of pointer
   printf("%zu\n", sizeof *p);// size of int
+}
+
+void arrays(void){
+  float f[4]; 
+
+}
+
+void bool_stuff(void){
+  bool x = true;
   if (x) {
     printf("x is true \n");
   }
+}
+
+int main (void){
+  int i = 2;   
+
 
 }
