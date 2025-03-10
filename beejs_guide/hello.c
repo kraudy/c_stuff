7cae5867f3b3cba88364361088e0ed62db0a5373
Roberto Torrez <robkraudy@gmail.com>
2025-03-10 06:52:05 -0600

    Print stuff
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/hello.c
+++ beejs_guide/hello.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void pointers(int* i){
-  int *p;
-  char *s = "Hello, world !";
-
-  p = i;
-
-  printf("%s i  = %d and f = %f \n", s, i);
-  printf("i addres: %p \n", (void *) p);
-  printf("%zu\n", sizeof p); //size of pointer
-  printf("%zu\n", sizeof *p);// size of int
-}
-
-void arrays(void){
-  float f[4]; 
-
-}
-
 void bool_stuff(void){
+  puts("\nbool_stuf");
   bool x = true;
   if (x) {
     printf("x is true \n");
   }
 }
 
+void pointers(int* i){
+  puts("\npointers");
+  int *p;
+  char *s = "Hello, world !";
+
+  p = i;
+
+  printf("%s i  = %d  \n", s, *i);
+  printf("i addres: %p \n", (void *) p);
+  printf("%zu\n", sizeof p); //size of pointer
+  printf("%zu\n", sizeof *p);// size of int
+}
+
+void arrays(void){
+  puts("\narrays");
+  float f[4]; 
+
+}
+
+
 int main (void){
   int i = 2;   
 
+  bool_stuff();
+  pointers(&i);
+  arrays();
 
 }
