d9c5e68a019dc3fc62a4bc776c7505aa44956890
Roberto Torrez <robkraudy@gmail.com>
2025-03-09 19:48:44 -0600

    Some pointer stuff
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/hello.c
+++ beejs_guide/hello.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main (void){
-  printf("Hello world\n");
+  bool x = true;
+  int i = 2; float f = 3.14;
+  int *p;
+  char *s = "Hello, world !";
+
+  p = &i;
+
+  printf("%s i  = %d and f = %f \n", s, i, f);
+  printf("i addres: %p \n", (void *) p);
+  printf("%zu\n", sizeof p); //size of pointer
+  printf("%zu\n", sizeof *p);// size of int
+  if (x) {
+    printf("x is true \n");
+  }
+
 }
