103fec12cab6e57e9953fad09bd4742dd8b51d5b
Roberto Torrez <robkraudy@gmail.com>
2025-03-13 21:26:49 -0600

    Write stuff to file
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/file_stuff.c
+++ beejs_guide/file_stuff.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void){
+int read_fopen(void){
   FILE *fp;
   char s[1024];
   int linecount = 0;
@@ -16,5 +16,41 @@ int main(void){
   puts("");
 
   fclose(fp);
+}
+
+int read_fscanf(void){
+  FILE *fp;
+  char name[1024];  // Big enough for any line this program will encounter
+  float length;
+  int mass;
+
+  fp = fopen("whales.txt", "r");
+
+  while (fscanf(fp, "%s %f %d", name, &length, &mass) != EOF)
+      printf("%s whale, %d tonnes, %.1f meters\n", name, mass, length);
+
+  fclose(fp);
+
+}
+
+int write_file(void){
+  FILE *fp;
+  int x = 32;
+
+  //fp = stdout;
+  fp = fopen("output.txt", "w");
+
+  fputc('B', fp);
+  fputc('\n', fp);
+  fprintf(fp, "x = %d\n", x);
+  fputs("Hello, world!\n", fp);
+
+  fclose(fp);
+}
+
+int main(void){
+  read_fopen();
+  read_fscanf();
+  write_file();
 
 }
