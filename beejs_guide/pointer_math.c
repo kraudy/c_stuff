6123552c6d92d28a54c905a90d84cc26499314c1
Roberto Torrez <robkraudy@gmail.com>
2025-03-14 20:52:38 -0600

    Some resize stuffl
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/pointer_math.c
+++ beejs_guide/pointer_math.c
@@ -18,7 +18,43 @@ void move_index(void){
 
 }
 
+void* my_memcpy(void *dest, void *src, int n_byte){
+  /* 
+  Convert void pointer to char, this help us to defference it and
+  conveniently a char = 1 byte so anything can be coppied
+  */
+  char *s = src, *d = dest;
+
+  while(n_byte)
+    *d++ = *s++;
+  
+  return dest;
+  
+}
+
+void resize_pointer(void){
+  float *p = malloc(20 * sizeof *p);
+  for (int i=0; i<20; i++)
+    *(p + i) = i;
+    //*(p + i) = *(&i);
+
+  float *new_p;
+  if((new_p = realloc(p, 40 * sizeof *p)) == NULL){
+    puts("Null pointer");
+  }
+
+  p = new_p;
+
+  for (int i=20; i<40; i++)
+    *(p + i) = i;
+
+  for (int i=0; i<40; i++)
+    printf("%f\n", *(p + i));
+
+  free(p);
+}
+
 int main(void){
   move_index();
-
+  resize_pointer();
 }
