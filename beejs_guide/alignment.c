bfd0e4666716a8e425101aa0bd58f02088b1d371
Roberto Torrez <robkraudy@gmail.com>
2025-03-16 08:16:33 -0600

    Create aligned realloc
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/alignment.c
+++ beejs_guide/alignment.c
@@ -2,7 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(void){
+void* aligned_realloc(void *ptr, size_t old_size, size_t alignment, size_t size){
+  char *new_ptr = aligned_alloc(alignment, size);
+  if (new_ptr == NULL)
+    return NULL;
+
+  size_t copy_size = old_size < size? old_size : size;  
+
+  if(ptr != NULL)
+    memcpy(new_ptr, ptr, copy_size);
+
+  free(ptr);
+
+  return new_ptr;
+}
+
+void try_aligned(void){
   char *p = aligned_alloc(64, 256); // 256 = 64*4
 
   //p = "Hello, world!";
@@ -10,5 +25,10 @@ int main(void){
   printf("%s\n", p);
 
   free(p);
+}
+
+
+int main(void){
+    try_aligned();
 }
 
