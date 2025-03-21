7a8ad2f39dfdab5cd64252a7a5499890ace92aa8
Roberto Torrez <robkraudy@gmail.com>
2025-03-21 07:16:34 -0600

    Some define substitution
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/define.c
+++ beejs_guide/define.c
@@ -20,7 +20,31 @@ void test2(void){
   #endif
 }
 
+void test3(void){
+  #if 0
+    puts("Shall never be known");
+  #endif
+  puts("Pedestrian text");
+}
+
+void test4(void){
+  printf("This function: %s\n", __func__);
+  printf("This file: %s\n", __FILE__);
+  printf("This line: %d\n", __LINE__);
+  printf("Compiled on: %s %s\n", __DATE__, __TIME__);
+  printf("C Version: %ld\n", __STDC_VERSION__);
+}
+
+#define SQR(x) x * x
+void test5(void){
+  printf("%d\n", SQR(12)); 
+  printf("%d\n", SQR(3 + 4)); // 19, bad! 
+}
+
 int main(void){
   test1();
   test2();
+  test3();
+  test4();
+  test5();
 }
