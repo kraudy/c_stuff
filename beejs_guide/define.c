dfb5b9e93b8211d7fec1cb72e065d689c6a5fa4e
Roberto Torrez <robkraudy@gmail.com>
2025-03-21 07:02:21 -0600

    Directive with if
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/define.c
+++ beejs_guide/define.c
@@ -2,10 +2,25 @@
 
 #define HAPPY
 
-int main(void){
+void test1(void){
   #ifndef HAPPY
     puts("not happy");
   #else 
     puts("happy");
   #endif
+}
+
+#define FACTOR 1
+
+void test2(void){
+  #if FACTOR == 1
+    puts("factor 1");
+  #elif FACTOR == 0
+    puts("factor 0");
+  #endif
+}
+
+int main(void){
+  test1();
+  test2();
 }
