93fe61de1adc71a51cec3b60ed2d58662ad08fe3
Roberto Torrez <robkraudy@gmail.com>
2025-03-22 17:48:17 -0600

    Basic self linked struct
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

---
+++ beejs_guide/linked_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct node {
+  //tensor T
+  int data;
+  struct node *next;
+  //uns n_prev;
+  //struct node *prev;
+};
+
+void test1(void){
+  struct node *head;
+  
+    
+
+}
+
+int main(void){
+  test1();
+}
+
