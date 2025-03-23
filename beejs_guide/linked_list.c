cea4d2856baac33a9195a3feb18c08c559e95ed3
Roberto Torrez <robkraudy@gmail.com>
2025-03-22 18:00:24 -0600

    Somo self init
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/linked_list.c
+++ beejs_guide/linked_list.c
@@ -10,9 +10,21 @@ struct node {
 
 void test1(void){
   struct node *head;
-  
-    
 
+  /* Interesting how a struct is a pointer to the first element, similar to an array*/
+  head = malloc(1 * sizeof(struct node));  
+  head->data = 11;
+  head->next = malloc(1 * sizeof(struct node));
+  head->next->data = 22;
+  head->next->next = malloc(1 * sizeof(struct node));
+  head->next->next->data = 33;
+  head->next->next->next = NULL;
+
+  for (struct node *cur = head; cur!=NULL; cur = cur->next)
+    printf("%d\n", cur->data);
+
+  for (struct node *cur = head; cur!=NULL; cur = cur->next)
+    free(cur->next); 
 }
 
 int main(void){
