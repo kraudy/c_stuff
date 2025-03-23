295bcd4f1435b58657ef21d6850756789233b5d4
Roberto Torrez <robkraudy@gmail.com>
2025-03-22 19:57:29 -0600

    Child test
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/linked_list.c
+++ beejs_guide/linked_list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct node {
   //tensor T
@@ -23,11 +24,38 @@ void test1(void){
   for (struct node *cur = head; cur!=NULL; cur = cur->next)
     printf("%d\n", cur->data);
 
-  for (struct node *cur = head; cur!=NULL; cur = cur->next)
-    free(cur->next); 
+  //for (struct node *cur = head; cur!=NULL; cur = cur->next)
+  //  free(cur->next); 
+}
+
+struct parent {
+    int a, b;
+};
+
+struct child {
+    struct parent super; 
+    int c, d;
+};
+
+void print_parent(void *p){
+  /* This let us defference the pointer */
+  struct parent *self = p;
+    printf("Parent: %d, %d\n", self->a, self->b);
+}
+
+void print_child(struct child *self){
+  printf("Child: %d, %d\n", self->c, self->d);
+}
+
+void child_test(void){
+  /* Initialize both structs */
+  struct child c = {.super.a=1, .super.b=2, .c=3, .d=4};
+  print_child(&c);
+  print_parent(&c);
 }
 
 int main(void){
   test1();
+  child_test();
 }
 
