4f9dd5c748dadb49439504b98bfa7eb5c61d35e0
Roberto Torrez <robkraudy@gmail.com>
2025-03-25 07:03:31 -0600

    More enum stuff
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/enums.c
+++ beejs_guide/enums.c
@@ -5,8 +5,21 @@ enum {
   TWO=2
 };
 
+enum {
+  SHEEP,
+  WHEAT,
+  WOOD,
+};
+
+enum {
+  x=2,
+  y=2,
+};
+
 void test1(void){
   printf("%d %d\n", ONE, TWO);
+  printf("%d %d %d\n", SHEEP, WHEAT, WOOD);
+  printf("%d %d\n", x, y);
 }
 
 int main(void){
