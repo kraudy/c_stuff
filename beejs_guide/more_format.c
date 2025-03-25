8816de1fd9cecb3ed8e1a9709aa090ee89e7179b
Roberto Torrez <robkraudy@gmail.com>
2025-03-25 06:44:08 -0600

    Nice counter
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

---
+++ beejs_guide/more_format.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <threads.h>
+
+int main(void){
+  for (int i = 5; i >= 0; i--) {
+    printf("\rT minus %d second%s... \b", i, i != 1? "s": "");
+
+    fflush(stdout);  // Force output to update
+
+    // Sleep for 1 second
+    thrd_sleep(&(struct timespec){.tv_sec=1}, NULL);
+  }
+
+  printf("\rLiftoff!             \n");
+}
