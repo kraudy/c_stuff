8184739f0c464d8358637c93ee0f6b326cc046b0
Roberto Torrez <robkraudy@gmail.com>
2025-03-25 21:23:02 -0600

    Show pointer
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/pointer_to_pointer_boy.c
+++ beejs_guide/pointer_to_pointer_boy.c
@@ -2,11 +2,19 @@
 
 void test1(void){
   int x = 3490;
-  void *p = &x;
-  int *pint = p;
+  int *p = &x;
+  int **q = &p;
+  void *pvoid = &x;
+  int *pint = pvoid;
 
-  printf("%d\n", *((int*)p)); 
+  printf("%d\n", **q); 
+  printf("%d\n", *((int*)pvoid)); 
   printf("%d\n", *pint); 
+  puts("Show addresses");
+  printf("%p\n", &x); 
+  printf("%p\n", p); 
+  printf("%p\n", &p); 
+  printf("%p\n", &*q); 
 }
 
 int main(void){
