79a1b0dc03b1d164405d167af8a07bc02716a9c6
Roberto Torrez <robkraudy@gmail.com>
2025-03-26 07:08:41 -0600

    Object representation
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/pointer_to_pointer_boy.c
+++ beejs_guide/pointer_to_pointer_boy.c
@@ -27,12 +27,32 @@ void* my_memcpy(void *dest, const void *src, size_t n){
   return dest;
 }
 
-int main(void){
-  test1();
+void test2(void){
   int a = 5;
   int b;
   void *s = &a;
   void *d = &b;
   my_memcpy(d, s, sizeof(int));
   printf("%d\n", b);
+}
+
+struct foo{
+  char a;
+  int b;
+};
+
+void test3(void){
+  struct foo x = {0x12, 0x12345678};
+  unsigned  char *p =  (unsigned char*)&x;
+
+  for (size_t i=0; i<sizeof x; i++){
+    printf("%02X\n", p[i]);
+  }
+
+}
+
+int main(void){
+  test1();
+  test2();
+  test3();
 }
