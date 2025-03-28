3c65787a3cd96da6f364e082a4a8095c4ea1d8eb
Roberto Torrez <robkraudy@gmail.com>
2025-03-28 13:57:05 -0600

    TCO stuff with gotos
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/gotos.c
+++ beejs_guide/gotos.c
@@ -11,6 +11,24 @@ skip_3:
   printf("Four \n");
 }
 
+// TCO Factorial
+int factorial(int n, int a){
+tco:
+  if (n==0)
+    return a;
+
+  int next_n = n-1;
+  int next_a = a*n;
+
+  n = next_n;
+  a = next_a;
+
+  goto tco;
+}
+
 int main(void){
   test1();
+  for(int i=0; i<8; i++){
+    printf("%d! == %d\n", i, factorial(i, 1));
+  }
 }
