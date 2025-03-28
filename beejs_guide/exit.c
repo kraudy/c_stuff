c8dd0076de4d932006e601d5b0bb9418dafc03ac
Roberto Torrez <robkraudy@gmail.com>
2025-03-27 20:25:29 -0600

    Quick exit
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- beejs_guide/exit.c
+++ beejs_guide/exit.c
@@ -9,10 +9,22 @@ void on_exit_2(void){
   printf("Exit handler 2 called!\n");
 }
 
-int main(void){
-
+void test1(void){
   atexit(on_exit_1);
   atexit(on_exit_2);
+}
+
+void on_quick_exit_1(void){
+  printf("Quick exit handler\n");
+}
+
+void test2(void){
+  at_quick_exit(on_quick_exit_1);
+}
+
+int main(void){
+  //test1();
+  test2();
 
   puts("End");
   return 0;
