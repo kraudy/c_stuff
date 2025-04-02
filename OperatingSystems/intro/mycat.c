ef1252f0ca681918ea63544357869cad8e408238
Roberto Torrez <robkraudy@gmail.com>
2025-04-02 13:53:33 -0600

    Reading with chars
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- OperatingSystems/intro/mycat.c
+++ OperatingSystems/intro/mycat.c
@@ -24,13 +24,28 @@ int read_file_fgets(char *name){
   return 0;
 }
 
+int read_file_fgetc(char *name){
+  FILE *f = NULL;
+  if (find_file(&f, name))
+    return 1;
+
+  printf("File: %s\n", name);
+  char c;
+  while((c = fgetc(f)) != EOF)
+    putchar(c);
+
+  fclose(f);
+  return 0;
+}
+
 int main(int argc, char *argv[]){
   assert(argc >= 2);
 
   puts("out");
 
   for (int i=1; i<argc; i++){
-    read_file_fgets(argv[i]);
+    //read_file_fgets(argv[i]);
+    read_file_fgetc(argv[i]);
     printf("\n\n");
   }
 
