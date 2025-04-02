a8ef183d69fa45acf1866f1fcb8e4100e34c6a93
Roberto Torrez <robkraudy@gmail.com>
2025-04-02 13:49:10 -0600

    Separate fin file logic
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- OperatingSystems/intro/mycat.c
+++ OperatingSystems/intro/mycat.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include <assert.h>
 
+int find_file(FILE **f, char *name){
+  *f = fopen(name, "r");
+  if(*f == NULL){
+    fprintf(stderr, "File not found: %s\n", name);
+    return 1;
+  }
+  return 0;
+}
+
+int read_file_fgets(char *name){
+  FILE *f = NULL;
+  if (find_file(&f, name))
+    return 1;
+
+  printf("File: %s\n", name);
+  char s[1024];
+  while(fgets(s, sizeof s, f) != NULL)
+    printf("%s", s);
+
+  fclose(f);
+  return 0;
+}
+
 int main(int argc, char *argv[]){
   assert(argc >= 2);
 
   puts("out");
 
   for (int i=1; i<argc; i++){
-    FILE *f = fopen(argv[i], "r");
-    if(f == NULL){
-      fprintf(stderr, "File not found: %s\n", argv[i]);
-      continue;
-    }
-    puts("found");
-    char s[1024];
-    while(fgets(s, sizeof s, f) != NULL)
-      printf("%s", s);
-
-    fclose(f);
+    read_file_fgets(argv[i]);
+    printf("\n\n");
   }
 
   return 0;
