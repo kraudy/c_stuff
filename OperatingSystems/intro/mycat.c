b76f68ec79c701178ffa9f33a07f3f1dcf51cd6d
Roberto Torrez <robkraudy@gmail.com>
2025-04-02 13:22:21 -0600

    Reading some files
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- OperatingSystems/intro/mycat.c
+++ OperatingSystems/intro/mycat.c
@@ -6,5 +6,19 @@ int main(int argc, char *argv[]){
 
   puts("out");
 
+  for (int i=1; i<argc; i++){
+    FILE *f = fopen(argv[i], "r");
+    if(f == NULL){
+      fprintf(stderr, "File not found: %s\n", argv[i]);
+      continue;
+    }
+    puts("found");
+    char s[1024];
+    while(fgets(s, sizeof s, f) != NULL)
+      printf("%s", s);
+
+    fclose(f);
+  }
+
   return 0;
 }
