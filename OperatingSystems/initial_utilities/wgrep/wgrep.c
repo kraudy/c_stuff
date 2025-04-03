9d64629cea816543b4e65bb162084de3c37d1868
Roberto Torrez <robkraudy@gmail.com>
2025-04-03 13:45:19 -0600

    Reading files with wgrep
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- OperatingSystems/initial_utilities/wgrep/wgrep.c
+++ OperatingSystems/initial_utilities/wgrep/wgrep.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+int find_file(FILE **fd, char *name){
+  if ((*fd = fopen(name, "r")) == NULL)
+    return 1;
+  
+  return 0;
+}
 
 int main(int argc, char *argv[]){
   if(argc == 1){
@@ -14,8 +19,22 @@ int main(int argc, char *argv[]){
   size_t len = 0;
   if(argc == 2){
     getline(&line, &len, stdin);
-    if (strstr(line, argv[2]) != NULL)
+    if (strstr(line, argv[1]) != NULL)
       fprintf(stdout, "%s",line);
+  }
+
+  for (int i = 2; i<argc; i++){
+    FILE *fd = NULL;
+    if(find_file(&fd, argv[i])){
+      fprintf(stdout, "wgrep: cannot open file\n");
+      return 1;
+    }
+
+    while((getline(&line, &len, fd)) != -1)
+      if ((strstr(line, argv[1])) != NULL)
+        fprintf(stdout, "%s", line);
+        
+    fclose(fd);
   }
 
   free(line);
