1f14a25514cde650f3e87fcbe51444085a9087b7
Roberto Torrez <robkraudy@gmail.com>
2025-04-03 06:56:50 -0600

    Fixin tests
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

---
+++ OperatingSystems/initial_utilities/wcat/test-wcat.sh
@@ -0,0 +1,8 @@
+#! /bin/bash  
+
+if ! [[ -x wcat ]]; then
+    echo "wcat executable does not exist"
+    exit 1
+fi
+
+../run-tests.sh $*
---
+++ OperatingSystems/initial_utilities/wcat/wcat.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <assert.h>
+
+int find_file(FILE **f, char *name){
+  *f = fopen(name, "r");
+  if(*f == NULL){
+    //fprintf(stderr, "File not found: %s\n", name);
+    return 1;
+  }
+  return 0;
+}
+
+int read_file_fgets(char *name){
+  FILE *f = NULL;
+  if (find_file(&f, name))
+    return 1;
+
+  //printf("File: %s\n", name);
+  char s[1024];
+  while(fgets(s, sizeof s, f) != NULL)
+    printf("%s", s);
+
+  fclose(f);
+  return 0;
+}
+
+int read_file_fgetc(char *name){
+  FILE *f = NULL;
+  if (find_file(&f, name))
+    return 1;
+
+  //printf("File: %s\n", name);
+  char c;
+  while((c = fgetc(f)) != EOF)
+    putchar(c);
+
+  fclose(f);
+  return 0;
+}
+
+int main(int argc, char *argv[]){
+  assert(argc >= 2);
+
+  for (int i=1; i<argc; i++){
+    //read_file_fgets(argv[i]);
+    if (read_file_fgetc(argv[i]))
+      return 0;
+    //printf("\n\n");
+  }
+
+  return 0;
+}
